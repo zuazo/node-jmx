0a0bd1f9c03f169c67716154a05c96155dd64b14
Xabier de Zuazo <xabier@onddo.com>
2013-04-29 13:19:04 +0200

    Simple JMX read method working

---
+++ index.js
@@ -0,0 +1,22 @@
+var Client = require('./lib/client'),
+    assert = require('assert');
+
+module.exports = {
+  createClient: createClient,
+  Client: Client
+};
+
+function createClient(options) { 
+  assert.ok(options, "options is required");
+  if (options.service) {
+    var serviceOrHost = options.service;
+  } else {
+    var serviceOrHost = options.host || 'localhost';
+    var protocol = options.protocol;
+    assert.ok(options.port, "port is required");
+    var port = options.port;
+    var urlPath = options.urlPath;
+  }
+  return new Client(serviceOrHost, port, protocol, urlPath);
+}
+
